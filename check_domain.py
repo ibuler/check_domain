779e139a03764e3d02d60a8764969f7383191d98
ibuler <ibuler@qq.com>
2016-02-19 19:07:42 +0800

    增加中断后从最后一次开始

--- check_domain.py
+++ check_domain.py
@@ -7,29 +7,37 @@
 import time
 import urllib
 import re
+import os
+import pickle
 
 api = "http://panda.www.net.cn/cgi-bin/check.cgi?area_domain=%s"  # api地址
-string = "abcdefghijklmnopqrstuvwxyz1234567890"                   # 所有字母
+string = "abcdefghigklmnopqrstuvwxyz"                                            # 所有字母
 string_len = len(string)                                          # 长度
 fname = 'name.txt'                                                # 还没被注册的域名写入该文件
-suffix = '.com'                                                   # 域名后缀
-domain_lenth_range = range(3, 5)                                  # 字母组合的长度，3到5但不包括5
+suffix = '.cn'                                                   # 域名后缀
+domain_lenth_range = range(4, 5)                                  # 字母组合的长度，3到5但不包括5
+last_pickle_f = 'last.pkl'
 
 
 def min(num):
     """初始化第一个值数字列表"""
     name = []
+    if os.path.isfile('min.txt'):
+        with open('min.txt') as f:
+            name.append(f.read().strip())
     for i in range(num):
         name.append(0)
     return name
 
+
 def max(num, max_num):
     """返回最大的值数字列表"""
-    name =  []
+    name = []
     for i in range(num):
         name.append(max_num)
     return name
-    
+
+
 def num_2_string(name, string):
     """将数字列表转化为字母组合列表"""
     new_name = []
@@ -37,7 +45,7 @@ def num_2_string(name, string):
         new_name.append(string[i])
     return ''.join(new_name)
 
-	
+
 def is_ava(domain):
     """判断该域名是否被注册"""
     data = urllib.urlopen(api % domain).read()
@@ -53,23 +61,35 @@ def is_ava(domain):
     else:
         print 'Forbidden'
         return False
-        
+
+
 def domain_name(num):
     """域名组合生成器"""
-    name = min(num)
+    if os.path.isfile(last_pickle_f):
+        with open(last_pickle_f) as f:
+            name = pickle.load(f)
+            if not isinstance(name, list):
+                name = min(num)
+    else:
+        name = min(num)
+
     last = max(num, string_len-1)
     while True:
-        yield num_2_string(name, string)
+        yield num_2_string(name, string)  # [1, 3 ,4] => [a, c, d]
         if name == last:
             break
-        name[num-1] += 1
+        name[num-1] += 1  # 最后一位+1
+        with open(last_pickle_f, 'w') as f:
+            pickle.dump(name, f)  # 保存结果供中断后作为最小使用
+
         while string_len in name:
             index = name.index(string_len)
             name[index] = 0
             name[index-1] += 1
-            
+
+
 def run(domain_lenth):
-    """执行，如果每被注册就写到文件中"""
+    """执行，如果没被注册就写到文件中"""
     f = open(fname, 'a')
     for domain in domain_name(domain_lenth):
         domain += suffix
